cbc31572ac48521f4e366300c77f9988046e6399
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2022-07-26 18:05:13 -0700

    seed database

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Note;
+use App\Models\NoteTag;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@email.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $note1 = Note::factory()->user($user)->create();
+        $note2 = Note::factory()->user($user)->create();
+        $note3 = Note::factory()->user($user)->create();
+
+        $tag1 = Tag::factory()->create(['name' => 'mathematics']);
+        $tag2 = Tag::factory()->create(['name' => 'history']);
+        $tag3 = Tag::factory()->create(['name' => 'science']);
+        $tag4 = Tag::factory()->create(['name' => 'english']);
+        $tag5 = Tag::factory()->create(['name' => 'engineering']);
+
+        NoteTag::factory()->note($note1)->tag($tag1)->create();
+        NoteTag::factory()->note($note1)->tag($tag2)->create();
+        NoteTag::factory()->note($note2)->tag($tag2)->create();
+        NoteTag::factory()->note($note2)->tag($tag3)->create();
+        NoteTag::factory()->note($note2)->tag($tag4)->create();
+        NoteTag::factory()->note($note3)->tag($tag4)->create();
+        NoteTag::factory()->note($note3)->tag($tag5)->create();
     }
+
 }
