0aafdeda88f83a26642ab9eb13eab36892428db8
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2022-07-26 18:02:50 -0700

    create migrations

---
+++ backend/database/migrations/2022_07_26_221209_create_note_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_tags', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('note_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+
+            $table->foreign('note_id')->references('id')->on('notes');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_tags');
+    }
+};
