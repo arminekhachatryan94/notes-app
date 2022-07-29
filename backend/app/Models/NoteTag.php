990be825f7271b4e89b5c9c2fe99a039eefe0449
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2022-07-28 21:44:40 -0700

    add notetag model relationships

--- backend/app/Models/NoteTag.php
+++ backend/app/Models/NoteTag.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class NoteTag extends Pivot
@@ -20,4 +21,14 @@ class NoteTag extends Pivot
         'note_id',
         'tag_id',
     ];
+
+    public function note(): BelongsTo
+    {
+        return $this->belongsTo(Note::class, 'note_id', 'id');
+    }
+
+    public function tag(): BelongsTo
+    {
+        return $this->belongsTo(Tag::class, 'tag_id', 'id');
+    }
 }
