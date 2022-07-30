27272bdb40f9ba1c5e1f499f4fe77bbb0c9ed6f3
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2022-07-29 22:52:37 -0700

    use react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import Notes from './Pages/Notes';
+import Tags from './Pages/Tags';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+      <Routes>
+        <Route path="/" element={<Notes/>} />
+        <Route path="/notes" element={<Notes/>} />
+        <Route path="/tags" element={<Tags/>} />
+      </Routes>
+  </>);
 }
 
 export default App;
